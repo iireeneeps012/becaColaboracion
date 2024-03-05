288d3ced05ef9ef00e2b77428f00ec47b7acc0a0
i.perezs.2020 <ireperezsant@outlook.es>
2024-03-05 16:30:13 +0100

    añadido pop

---
+++ PantallaSoloAudio/scripts/cambiosPantalla.js
@@ -0,0 +1,36 @@
+// Para ir a la pantalla de solo audio
+document.addEventListener('DOMContentLoaded', function() {
+    var clickBtnAudio = document.getElementById('btn_audio');
+    clickBtnAudio.addEventListener('click', function() {
+        window.location.href = '../PantallaSoloAudio/index.html';
+    });
+});
+
+
+// Para ir a la pantalla de proyectar
+document.addEventListener('DOMContentLoaded', function() {
+    var clickBtnProyector = document.getElementById('btn_proyector');
+    clickBtnProyector.addEventListener('click', function() {
+        window.location.href = '../PantallaProyectar/index.html';
+    });
+});
+
+
+// Para ir a la pantalla de videoconferencia
+document.addEventListener('DOMContentLoaded', function() {
+    var clickBtnVideoconferencia = document.getElementById('btn_video');
+    clickBtnVideoconferencia.addEventListener('click', function() {
+        window.location.href = '../PantallaVideoconferencia/index.html';
+    });
+});
+
+
+// para ir a la pantalla de configuración de audio
+
+
+
+
+
+// Para ir a control manual
+
+
--- prueba/pantallaPopUp/script.js
+++ prueba/pantallaPopUp/script.js
@@ -1,10 +1,10 @@
 window.addEventListener("load", function() {
-    document.querySelector(".popup").style.display = "block"; // Mostrar el popup automáticamente cuando se carga la página
+    document.querySelector(".popupCarga").style.display = "block"; // Mostrar el popup automáticamente cuando se carga la página
     setTimeout(function() {
-        document.querySelector(".popup").style.display = "none"; // Ocultar el popup después de 10 segundos
+        document.querySelector(".popupCarga").style.display = "none"; // Ocultar el popup después de 10 segundos
     }, 10000);
 });
 
 document.querySelector("#close").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popupCarga").style.display = "none";
 });
