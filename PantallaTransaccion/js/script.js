a8129449c842f9be4d06d80c906540fc1a3dc498
i.perezs.2020 <ireperezsant@outlook.es>
2024-03-19 16:18:23 +0100

    se cambia a local los icons

--- PantallaTransaccion/js/script.js
+++
@@ -1,8 +0,0 @@
-// Función para cerrar la ventana emergente y otras ventanas del mismo proyecto
-function cerrarVentanas() {
-    // Cierra la ventana emergente
-    window.close();
-}
-
-// Llama a la función para cerrar la ventana emergente y otras ventanas después de 5000 milisegundos (5 segundos)
-setTimeout(cerrarVentanas, 5000);
